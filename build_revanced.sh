ee150b13fa3036ecef7149f22c2449a734780fc7
LeddaZ <leonardoledda@gmail.com>
2022-06-23 23:39:25 +0200

    Skip building YouTube Music

--- build_revanced.sh
+++ build_revanced.sh
@@ -75,19 +75,19 @@ then
 else
     echo "Cannot find YouTube APK, skipping build"
 fi
-echo ""
-echo "************************************"
-echo "Building YouTube Music APK"
-echo "************************************"
-if [ -f "com.google.android.apps.youtube.music.apk" ]
-then
-    echo "Building Root APK"
-    java -jar revanced-cli.jar -b revanced-patches.jar --mount \
-                               -e microg-support \
-                               -a com.google.android.apps.youtube.music.apk -o build/revanced-music-root.apk
-    echo "Building Non-root APK"
-    java -jar revanced-cli.jar -b revanced-patches.jar \
-                               -a com.google.android.apps.youtube.music.apk -o build/revanced-music-nonroot.apk
-else
-    echo "Cannot find YouTube Music APK, skipping build"
-fi
+# echo ""
+# echo "************************************"
+# echo "Building YouTube Music APK"
+# echo "************************************"
+# if [ -f "com.google.android.apps.youtube.music.apk" ]
+# then
+#     echo "Building Root APK"
+#     java -jar revanced-cli.jar -b revanced-patches.jar --mount \
+#                                -e microg-support \
+#                                -a com.google.android.apps.youtube.music.apk -o build/revanced-music-root.apk
+#     echo "Building Non-root APK"
+#     java -jar revanced-cli.jar -b revanced-patches.jar \
+#                                -a com.google.android.apps.youtube.music.apk -o build/revanced-music-nonroot.apk
+# else
+#     echo "Cannot find YouTube Music APK, skipping build"
+# fi
