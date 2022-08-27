d9125c096ecc71c53440d7b617578c8019eb7fcc
LeddaZ <leonardoledda@gmail.com>
2022-08-27 16:44:58 +0200

    workflows/sha256gen: Split YT and YTM hashes

--- scripts/sha256gen.sh
+++ scripts/sha256gen.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-# Generate SHA-256 hash
-sha256sum build/*signed.apk > build/SHA-256.txt
+music=no
+revanced=no
+while getopts mr flag
+do
+    case "${flag}" in
+        m) music=yes;;
+        r) revanced=yes;;
+    esac
+done
+
+# Generate SHA-256 hashes
+if [ "$revanced" = 'yes' ]; then
+    sha256sum build/revanced-nonroot-signed.apk > build/SHA-256-yt.txt
+fi
+
+if [ "$music" = 'yes' ]; then
+    sha256sum build/revanced-music-nonroot-*-signed.apk > build/SHA-256-ytm.txt
+fi
