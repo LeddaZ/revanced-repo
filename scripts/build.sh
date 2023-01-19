02242c980a807f62258378f3e3fa7f9a923ff215
LeddaZ <leonardoledda@gmail.com>
2023-01-19 01:10:08 +0100

    scripts: build: Update integrations artifact name

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,7 +16,7 @@ VMG_VERSION="0.2.24.220220"
 declare -A artifacts
 
 artifacts["revanced-cli.jar"]="revanced/revanced-cli revanced-cli .jar"
-artifacts["revanced-integrations.apk"]="revanced/revanced-integrations app-release-unsigned .apk"
+artifacts["revanced-integrations.apk"]="revanced/revanced-integrations revanced-integrations .apk"
 artifacts["revanced-patches.jar"]="revanced/revanced-patches revanced-patches .jar"
 artifacts["apkeep"]="EFForg/apkeep apkeep-x86_64-unknown-linux-gnu"
 
